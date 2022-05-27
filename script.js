0ca4649387023afca4bb47c9e8196f06b37f3df2
EatOrangeChicken <106408185+EatOrangeChicken@users.noreply.github.com>
2022-05-27 18:48:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-document.getElementById('switch').onclick = function() {
+document.getElementById('switch').onclick = function change() {
   if (document.getElementById('theme').href == "style.css") {
     document.getElementById('theme').href = "orange.css";
   } else {
     document.getElementById('theme').href = "style.css";
   }
 };
+change();
